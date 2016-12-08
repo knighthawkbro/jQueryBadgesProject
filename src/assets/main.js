1c405ca524cb2dc88cf6a42dade7c4b447d26652
Brady Walsh <walshb8@wit.edu>
2016-12-08 17:40:33 -0500

    Finished The Build

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
 
   // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/knighthawkbro.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle the response here
+      $.each(response.courses.completed, function(course) {
+        var badge = $('#badges').append("<div class='course'>"+
+        "<h3>"+this.title+"</h3>"+
+        "<img src="+this.badge+">"+
+        "<a href="+this.url+" target='_blank' class='btn btn-primary'>See Course</a>"+
+        "</div>");
+      });
+    }
+  });
 });
